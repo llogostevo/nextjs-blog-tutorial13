701a96d74a204c8af71cd95e381e1b57bb889eb7
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-06-10 10:42:42 +0100

    comments into the posts library file

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,7 +1,53 @@
-import { fstat } from "fs";
+import fs from "fs";
 import path from "path";
 import matter from 'gray-matter'
 
-const postsDrectory = path.join(process.cwd(), 'blogposts')
+const postsDirectory = path.join(process.cwd(), 'blogposts')
 
-export function getSortedPostsData() {}
+//declared with the export  so can be imported into other modules
+export function getSortedPostsData() {
+    //get file names under /posts
+    //read all filenames in the postsDirectory
+    //synchronously with fs.readdirSync(postsDirectory). The resulting array of filenames is stored in fileNames.
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames.map((filename) => {
+        //remove .md from filename to get id
+        // using regex and replace method looping through each array element
+        const id = filename.replace(/\.md$/, "");
+
+        // builds the full path to the markdown file
+        const fullPath = path.join(postsDirectory, filename);
+        // read markdown file as a string
+        const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+        //use gray-matter to parse the post metadata section
+        //resulting object is stored in matterResult. This object typically contains a data property which holds all the metadata (YAML front matter) defined in the markdown file.
+        const matterResult = matter(fileContents);
+
+        /*
+            Creates a new object blogPost with properties id, title, and date. 
+            The id comes from the filename, and title and date are obtained from the metadata parsed by gray-matter
+        */
+        const blogPost: BlogPost = {
+            id,
+            title: matterResult.data.title, 
+            date: matterResult.data.date, 
+        }
+        
+        /*
+            returns the blogPost object. The result is an array of these objects, stored in allPostsData.
+        */
+        return blogPost;
+    });
+
+    // use of a ternary statement to return 1 if true and -1 if false
+
+    /*
+        sorts the allPostsData array by the date property in descending order, using the sort() method. 
+        The sort comparison function (a, b) => a.date < b.date ? 1 : -1 returns 1 if a.date is less than b.date (i.e., a is more recent than b), and -1 otherwise. 
+        This effectively sorts the posts from the most recent to the oldest.
+        function finally returns the sorted array of blog post data.
+    */
+    return allPostsData.sort((a, b) => a.date <b.date ? 1 : -1);
+    
+}
